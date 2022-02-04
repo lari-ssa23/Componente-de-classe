f423d7997634cb75e4e2530938f82d0edbf15013
Larissa Isabel <91093355+lari-ssa23@users.noreply.github.com>
2022-02-04 20:24:00 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//criar um componente de class e exibir uma frase na tela
+import React, { Component } from "react";
+class Hidrate extends Component {
+render(){
+return(
+<div>
+<h1>Beba agua para hidratar seu corpinho</h1>
+</div>
+);
+}
+}
+export default Hidrate
+
